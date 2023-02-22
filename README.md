1b9451fb9154683a9e26a18fbbe76d383e295d01
Jody Phelan <jody.phelan@lshtm.ac.uk>
2023-02-22 18:00:48 +0000

    updates

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # penicillin-pneumo-rf
+
+This repository hosts the docker image file and data files for a prediction container that predicts penicillin resistance from *S. pneumoniae* whole genome sequencing data.
+It uses data featured in the [pyseer example](https://pyseer.readthedocs.io/en/master/tutorial.html). It is a proof of principal to show how [tb-ml](https://github.com/jodyphelan/tb-ml) can be extended to run on non-TB data. It takes a couple of shortcuts and shouldn't be used in production.
+
+Specifically it:
+* Sets any missing positions to reference
+
+It requires a vcf file as input and can be run through `tb-ml` with:
+
+```
+tb-ml --container jodyphelan/penicillin-pneumo-rf:latest "--vcf yoursample.vcf.gz"
+```
+
+For example you can use the test data in this repository:
+
+```
+tb-ml --container jodyphelan/penicillin-pneumo-rf:latest "--vcf /example/test.vcf.gz"
+```
+
+This conainer can be chaned to any pre-processing container that writes a vcf file.
--- scripts/predict_outcome.py
+++ scripts/predict_outcome.py
@@ -10,7 +10,6 @@ import subprocess as sp
 parser = argparse.ArgumentParser(description='Predict outcome from VCF file')
 parser.add_argument('--vcf', help='VCF file', required=True)
 parser.add_argument('--model', help='model file', required=True)
-parser.add_argument('--out', help='output file', required=True)
 args = parser.parse_args()
 
 # load model
@@ -22,7 +21,7 @@ positions = items['positions']
 
 # extract genotypes
 genos = {}
-for l in sp.Popen(r"bcftools query -f '%POS[\t%GT\n]' "+ "test.vcf.gz",shell=True,stdout=sp.PIPE).stdout:
+for l in sp.Popen(r"bcftools query -f '%POS[\t%GT\n]' "+ args.vcf,shell=True,stdout=sp.PIPE).stdout:
     row = l.decode().strip().split()
     if row[1]!="0/0":
         genos[int(row[0])] = 1
@@ -38,5 +37,4 @@ genos = [[genos[p] for p in positions]]
 # predict outcome
 pred = clf.predict(genos)
 # write output
-with open(args.out, "w") as f:
-    f.write("drug,prediction\npenicillin,%s\n" % pred[0])
+sys.stdout.write("drug,prediction\npenicillin,%s\n" % pred[0])
